2541e5c9994cca4f0cc1a45a750714ce403197bf
Catherine Bronzo <cbronzo@mac.com>
2018-06-17 01:29:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+katz_deli = []
+
+
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else 
+      katz_deli.each_with_index do |name, index|
+    puts "The line is currently: #{index.to_i+1}. #{name}"
+      end
+  end
+end
+
+
+
