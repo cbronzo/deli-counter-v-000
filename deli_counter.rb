7cbedff8361c5e81c4b9222c9a06ce58be731d28
Catherine Bronzo <cbronzo@mac.com>
2018-06-17 01:53:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def take_a_number(katz_deli,name)
   
   def now_serving(katz_deli)
     if katz_deli.length != 0
-      puts "Currently serving #{katz_deli[0]}"
+      puts "Currently serving #{katz_deli[0]}."
       array.pop
    else
      puts "There is nobody waiting to be served!"
