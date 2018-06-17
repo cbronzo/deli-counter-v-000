1eae256ec046bb137fbfe0f55140120816e20b06
Catherine Bronzo <cbronzo@mac.com>
2018-06-17 01:45:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ def line(katz_deli)
   end
 end
 
-def take_a_number('name', katz_deli)
+def take_a_number(name, katz_deli)
     puts "Welcome, #{"name"}. You are number #{katz_deli[]} in line."
   end
   
